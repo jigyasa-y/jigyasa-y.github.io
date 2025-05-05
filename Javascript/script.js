720eb6f958409f414f398a3541a488c673ae69f9
jigyasa-y <jigyasayadav361@gmail.com>
2025-05-05 11:52:10 +0530

    Update script.js

--- Javascript/script.js
+++ Javascript/script.js
@@ -87,22 +87,19 @@ const open=document.querySelector('#navList');
 const close=document.querySelector('#close_image');
 const navigation =document.querySelector(".navigators");
 let isDisplay=false;
-open.addEventListener('click',()=>{
-    if(isDisplay){
-        navigation.classList.remove('pop_up');
-        open.classList.remove('shift');
-        open.classList.add('open_image');
+  open.addEventListener('click',(e)=>{
+    e.stopPropagation();
+ navigation.classList.toggle('pop_up');
+ open.classList.toggle('shift');
+ open.classList.toggle('open_image');
+  
+  })
+
+  document.addEventListener('click',()=>{
+    if(navigation.classList.contains('pop_up')){
+      navigation.classList.remove('pop_up');
+      open.classList.remove('shift');
+      open.classList.add('open_image');
       
-        isDisplay=false;
     }
-    else{
-
-        navigation.classList.add('pop_up');
-        open.classList.remove('open_image');
-        open.classList.add('shift');
-       
-    
-        isDisplay=true;
-    }
-
-})
+  })
